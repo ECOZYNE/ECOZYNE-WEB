00a343d96fd98fd842a0dc2a03d7445a247d3b04
Ambaadalahaku <justinozon53@gmail.com>
2025-04-11 20:50:29 +0700

    Justine Mengedit halaman index student

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -45,3 +45,30 @@ fit=no">
          <a href="/student/edit/{{ $data->nim }}"  class="btn btn-sm  
          btn-warning mr-1"> <i class="fas fa-edit mr-2"></i> Edit</a> 
          <form method="POST" action="/student/delete/{{  $data->nim }}">
+         @csrf @method('DELETE')
+        <button type="submit" class="btn btn-sm btn-danger mr-1"><i class="fas
+        fa-trash-alt mr-2"></i> Hapus</button>
+        </form>
+        </td>
+        </tr>
+        @empty
+ <tr>
+ <td rowspan="100%">Tidak ada data untuk ditampilkan !</td>
+ </tr>
+ @endforelse
+ </tbody>
+ </table>
+ </div>
+ </div>
+ </div>
+ </div>
+ </div>
+ <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+ <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+ <script src="https://cdnjs.cloudflare.
+ com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+ <script src="https://stackpath.bootstrapcdn.
+ com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+ <script src="/js/bootstrap.min.js"></script>
+</body>
+</html>
