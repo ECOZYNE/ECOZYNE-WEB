96a5334779f957030cda9d032310fc2a6dbc9dbd
nabil <nabiladitya2203@gmail.com>
2025-04-26 23:29:04 +0700

    Create TransaksiPenukaranController.php

---
+++ app/Http/Controllers/TransaksiPenukaranController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\transaksi_penukaran;
+use Illuminate\Http\Request;
+
+class TransaksiPenukaranController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(transaksi_penukaran $transaksi_penukaran)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(transaksi_penukaran $transaksi_penukaran)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, transaksi_penukaran $transaksi_penukaran)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(transaksi_penukaran $transaksi_penukaran)
+    {
+        //
+    }
+}
