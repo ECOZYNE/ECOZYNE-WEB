aba41e7d71a1b254539b1ec90b9cbd9b94d51017
nabil <nabiladitya2203@gmail.com>
2025-04-26 23:26:35 +0700

    Create BankSampahController.php

---
+++ app/Http/Controllers/BankSampahController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BankSampah;
+use Illuminate\Http\Request;
+
+class BankSampahController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(BankSampah $bankSampah)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(BankSampah $bankSampah)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, BankSampah $bankSampah)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(BankSampah $bankSampah)
+    {
+        //
+    }
+}
