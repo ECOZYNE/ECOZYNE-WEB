8661483442965d8b4ae9cd530be6f6c09acde524
nabil <nabiladitya2203@gmail.com>
2025-04-26 23:29:13 +0700

    Create TransaksiProdukController.php

---
+++ app/Http/Controllers/TransaksiProdukController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\transaksi_produk;
+use Illuminate\Http\Request;
+
+class TransaksiProdukController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(transaksi_produk $transaksi_produk)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(transaksi_produk $transaksi_produk)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, transaksi_produk $transaksi_produk)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(transaksi_produk $transaksi_produk)
+    {
+        //
+    }
+}
