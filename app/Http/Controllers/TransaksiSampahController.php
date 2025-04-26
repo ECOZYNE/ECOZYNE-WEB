8db993ff076ba554d766d235647f1ce7df6f114b
nabil <nabiladitya2203@gmail.com>
2025-04-26 23:29:22 +0700

    Create TransaksiSampahController.php

---
+++ app/Http/Controllers/TransaksiSampahController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\transaksi_sampah;
+use Illuminate\Http\Request;
+
+class TransaksiSampahController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(transaksi_sampah $transaksi_sampah)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(transaksi_sampah $transaksi_sampah)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, transaksi_sampah $transaksi_sampah)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(transaksi_sampah $transaksi_sampah)
+    {
+        //
+    }
+}
