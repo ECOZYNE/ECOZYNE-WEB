a8763664bab08095743f018202479bfa9d23154b
nabil <nabiladitya2203@gmail.com>
2025-04-26 23:29:42 +0700

    Create BankSampah.php

---
+++ app/Models/BankSampah.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BankSampah extends Model
+{
+   
+    protected $table = 'bank_sampah';
+    protected $primaryKey = 'id_bank_sampah';
+
+}
