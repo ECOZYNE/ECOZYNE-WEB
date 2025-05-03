33ef3b72ac7dc56c85fc94cce3b8b0dbcd19b13a
nabil <nabiladitya2203@gmail.com>
2025-05-04 00:10:39 +0700

    Update add-komunitas.blade.php

--- resources/views/admin/add-komunitas.blade.php
+++ resources/views/admin/add-komunitas.blade.php
@@ -34,87 +34,101 @@
           <!-- Formulir Pendaftaran -->
           <form method="POST" action="/register-post">
             @csrf
-            <div class="container-fluid">
-              <div class="row">
-                <div class="col-md-4 mb-3">
-                  <label for="namaLengkap" class="form-label">Nama Lengkap</label>
-                  <input type="text" class="form-control" name="nama" id="namaLengkap"
-                    placeholder="Masukkan nama lengkap" required>
-                </div>
-                <div class="col-md-4 mb-3">
-                  <label for="email" class="form-label">Email</label>
-                  <input type="email" class="form-control" name="email" id="email" placeholder="Masukkan email"
-                    required>
-                </div>
-                <div class="col-md-4 mb-3">
-                  <label for="noTelp" class="form-label">No Telp</label>
-                  <input type="text" class="form-control" name="no_telp" id="noTelp"
-                    placeholder="Masukkan nomor telepon" required minlength="12" maxlength="12">
-                </div>
-
-                <div class="col-md-4 mb-3">
-                  <label for="kecamatan" class="form-label">Kecamatan</label>
-                  <select class="form-control" name="kecamatan" id="kecamatan" required>
-                    <option value="" disabled selected>Pilih Kecamatan</option>
-                    <option value="batu ampar">Batu Ampar</option>
-                    <option value="bengkong">Bengkong</option>
-                    <option value="lubuk baja">Lubuk Baja</option>
-                    <option value="sekar batu">Sekupang</option>
-                    <option value="batam kota">Batam Kota</option>
-                    <option value="sei beduk">Sei Beduk</option>
-                    <option value="sagulung">Sagulung</option>
-                    <option value="batu aji">Batu Aji</option>
-                    <option value="nongsa">Nongsa</option>
-                    <option value="galang">Galang</option>
-                    <option value="bulang">Bulang</option>
-                    <option value="belakang padang">Belakang Padang</option>
-                  </select>
-                </div>
-                <div class="col-md-8 mb-3">
-                  <label for="alamat" class="form-label">Alamat</label>
-                  <textarea class="form-control" name="alamat" id="alamat" rows="3"
-                    placeholder="Masukkan alamat lengkap" required></textarea>
-                </div>
+            <div class="row">
+              <div class="col-md-4 mb-3">
+                <label for="namaLengkap" class="form-label">Nama Lengkap</label>
+                <input type="text" class="form-control" name="nama" id="namaLengkap" placeholder="Masukkan nama lengkap" required>
               </div>
+          
+              <div class="col-md-4 mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" name="email" id="email" placeholder="Masukkan email" required>
+              </div>
+          
+              <div class="col-md-4 mb-3">
+                <label for="noTelp" class="form-label">No Telp</label>
+                <input type="text" class="form-control" name="no_telp" id="noTelp" placeholder="Masukkan nomor telepon" required minlength="12" maxlength="12">
+              </div>
+          
+              <div class="col-md-4 mb-3">
+                <label for="kecamatan" class="form-label">Kecamatan</label>
+                <select id="kecamatan" name="kecamatan" class="form-control" required>
+                  <option value="">-- Pilih Kecamatan --</option>
+                  @foreach($kecamatan as $item)
+                    <option value="{{ $item->id_kecamatan }}">{{ $item->kecamatan }}</option>
+                  @endforeach
+                </select>
+              </div>
+          
+              <div class="col-md-4 mb-3">
+                <label for="kelurahan" class="form-label">Kelurahan</label>
+                <select id="kelurahan" name="kelurahan" class="form-control" required>
+                  <option value="">-- Pilih Kelurahan --</option>
+                </select>
+              </div>
+          
+              <div class="col-md-4 mb-3">
+                <label for="kode_pos" class="form-label">Kode Pos</label>
+                <input type="number" class="form-control" name="kode_pos" id="kode_pos" placeholder="Masukkan kode pos" required min="10000" max="99999">
+              </div>
+          
+              <div class="col-md-12 mb-3">
+                <label for="alamat" class="form-label">Alamat Lengkap</label>
+                <textarea class="form-control" name="alamat" id="alamat" rows="3" placeholder="Masukkan alamat lengkap" required></textarea>
+              </div>
+          
               <hr>
-              <div class="row">
-                <div class="col-md-6 mb-3">
-                  <label for="namaPengguna" class="form-label">Nama Pengguna</label>
-                  <input type="text" class="form-control" name="username" id="namaPengguna"
-                    placeholder="Masukkan nama pengguna" required>
+          
+              <div class="col-md-6 mb-3">
+                <label for="namaPengguna" class="form-label">Nama Pengguna</label>
+                <input type="text" class="form-control" name="username" id="namaPengguna" placeholder="Masukkan nama pengguna" required>
+              </div>
+          
+              <div class="col-md-6 mb-3">
+                <label for="kataSandi" class="form-label">Kata Sandi</label>
+                <div class="input-group">
+                  <input type="password" class="form-control" name="password" id="kataSandi" placeholder="Masukkan kata sandi" required>
+                  <button type="button" class="btn btn-outline-secondary" onclick="togglePassword()">
+                    <i id="eyeIcon" class="bi bi-eye-slash"></i>
+                  </button>
                 </div>
-
-                <div class="col-md-6 mb-3">
-                  <label for="kataSandi" class="form-label">Kata Sandi</label>
-                  <div class="input-group">
-                    <input type="password" class="form-control" name="password" id="kataSandi"
-                      placeholder="Masukkan kata sandi" required>
-                    <button type="button" class="btn btn-outline-secondary" onclick="togglePassword()">
-                      <i id="eyeIcon" class="bi bi-eye-slash"></i>
-                    </button>
-                  </div>
-                </div>
-
-                <script>
-                  function togglePassword() {
-                    var passwordField = document.getElementById("kataSandi");
-                    var eyeIcon = document.getElementById("eyeIcon");
-
-                    if (passwordField.type === "password") {
-                      passwordField.type = "text";
-                      eyeIcon.classList.remove("bi-eye-slash");
-                      eyeIcon.classList.add("bi-eye");
-                    } else {
-                      passwordField.type = "password";
-                      eyeIcon.classList.remove("bi-eye");
-                      eyeIcon.classList.add("bi-eye-slash");
-                    }
-                  }
-                </script>
+              </div>
+            </div>
+          
+            <button type="submit" class="btn btn-primary w-100 py-8 fs-4 mb-4 mt-4 rounded-2">Daftar</button>
+          </form>
+          
+          <script>
+            document.getElementById('kecamatan').addEventListener('change', function () {
+              var kecamatanId = this.value;
+          
+              fetch('/get-kelurahan/' + kecamatanId)
+                .then(response => response.json())
+                .then(data => {
+                  var kelurahanSelect = document.getElementById('kelurahan');
+                  kelurahanSelect.innerHTML = '<option value="">-- Pilih Kelurahan --</option>';
+                  data.forEach(function (kelurahan) {
+                    kelurahanSelect.innerHTML += `<option value="${kelurahan.id_kelurahan}">${kelurahan.kelurahan}</option>`;
+                  });
+                });
+            });
+          
+            function togglePassword() {
+              var passwordField = document.getElementById("kataSandi");
+              var eyeIcon = document.getElementById("eyeIcon");
+              if (passwordField.type === "password") {
+                passwordField.type = "text";
+                eyeIcon.classList.remove("bi-eye-slash");
+                eyeIcon.classList.add("bi-eye");
+              } else {
+                passwordField.type = "password";
+                eyeIcon.classList.remove("bi-eye");
+                eyeIcon.classList.add("bi-eye-slash");
+              }
+            }
+          </script>          
 
               </div>
-
-              <button type="submit" class="btn btn-primary w-100 py-8 fs-4 mb-4 mt-4 rounded-2">Daftar</button>
             </div>
           </form>
 
