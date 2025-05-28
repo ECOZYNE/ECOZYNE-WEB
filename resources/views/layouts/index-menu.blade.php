d7561b30a5f7c123bcc8e2fba322607928979222
nabil <nabiladitya2203@gmail.com>
2025-05-28 16:09:22 +0700

    Create index-menu.blade.php

---
+++ resources/views/layouts/index-menu.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Ecozyne | Eco Enzyme Network')</title>
+
+    <!-- SEO -->
+    <meta name="description" content="Ecozyne - Sistem Pengelolaan Sampah dan Komunitas Berkelanjutan">
+    <meta name="keywords" content="ecozyne, pengelolaan sampah, komunitas berkelanjutan, e-commerce">
+    <meta name="author" content="Ecozyne Team">
+
+    <!-- Favicons -->
+    <link rel="shortcut icon" type="image/png" href="{{ asset('assets/images/logos/ecozyne.png') }}">
+    <link href="{{ asset('assets/img/favicon.png') }}" rel="icon">
+    <link href="{{ asset('assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com" rel="preconnect">
+    <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Poppins&family=Nunito&display=swap"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="{{ asset('assets2/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets2/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets2/vendor/aos/aos.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets2/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets2/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+
+    <!-- Main CSS File -->
+    <link href="{{ asset('assets2/css/main.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/styles-index-menu.css') }}" rel="stylesheet">
+
+    <link rel="shortcut icon" type="image/png" href="../assets/images/logos/ecozyne.png" />
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Font Awesome untuk ikon -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
+
+    @stack('style')
+</head>
+
+<body class="blog-page">
+
+    <x-loader />
+    <x-nav-header />
+
+    <main class="main">
+        @yield('content')
+    </main>
+
+    <x-footer />
+
+    <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center">
+        <i class="bi bi-arrow-up-short"></i>
+    </a>
+
+    <!-- Vendor JS Files -->
+    <script src="{{ asset('assets2/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets2/vendor/php-email-form/validate.js') }}"></script>
+    <script src="{{ asset('assets2/vendor/aos/aos.js') }}"></script>
+    <script src="{{ asset('assets2/vendor/glightbox/js/glightbox.min.js') }}"></script>
+    <script src="{{ asset('assets2/vendor/purecounter/purecounter_vanilla.js') }}"></script>
+    <script src="{{ asset('assets2/vendor/imagesloaded/imagesloaded.pkgd.min.js') }}"></script>
+    <script src="{{ asset('assets2/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+    <script src="{{ asset('assets2/vendor/swiper/swiper-bundle.min.js') }}"></script>
+
+    <!-- Main JS File -->
+    <script src="{{ asset('assets2/js/main.js') }}"></script>
+
+    @stack('scripts')
+
+</body>
+
+</html>
