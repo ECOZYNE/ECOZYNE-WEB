2c41b6432afef7f70ac17aeee343bea59505af38
nabil <nabiladitya2203@gmail.com>
2025-05-28 22:22:10 +0700

    Update BankSampah.php

--- app/Models/BankSampah.php
+++ app/Models/BankSampah.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\PengajuanBankSampah;
 use Illuminate\Database\Eloquent\Model;
 
 class BankSampah extends Model
@@ -10,4 +11,10 @@ class BankSampah extends Model
     protected $table = 'bank_sampah';
     protected $primaryKey = 'id_bank_sampah';
 
+    public function pengajuanBankSampah()
+    {
+        return $this->belongsTo(PengajuanBankSampah::class, 'id_pengajuan_bank_sampah', 'id_pengajuan_bank_sampah');
+    }
+
 }
+
