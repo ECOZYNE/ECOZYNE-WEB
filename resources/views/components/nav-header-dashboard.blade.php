4d7508e28a33224661b2b1f16242449a0720aaf9
nabil <nabiladitya2203@gmail.com>
2025-05-28 16:17:45 +0700

    Create nav-header-dashboard.blade.php

---
+++ resources/views/components/nav-header-dashboard.blade.php
@@ -0,0 +1,36 @@
+<!-- Header -->
+<header class="app-header shadow">
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <ul class="navbar-nav">
+      <li class="nav-item d-block d-xl-none">
+        <a class="nav-link sidebartoggler nav-icon-hover" id="headerCollapse" href="javascript:void(0)">
+          <i class="ti ti-menu-2"></i>
+        </a>
+      </li>
+    </ul>
+    <div class="navbar-collapse justify-content-end px-0" id="navbarNav">
+      <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-end">
+        <li class="nav-item dropdown">
+          <a class="nav-link nav-icon-hover" href="javascript:void(0)" id="drop2" data-bs-toggle="dropdown"
+            aria-expanded="false">
+            <img src="{{ asset('assets/images/profile/users.png') }}" alt="" width="35" height="35"
+              class="rounded-circle">
+          </a>
+          <div class="dropdown-menu dropdown-menu-end dropdown-menu-animate-up" aria-labelledby="drop2">
+            <div class="message-body">
+            <a href="{{ session('role') === 'admin' ? url('admin/my-profile') : url('dashboard/my-profile') }}"
+               class="d-flex align-items-center gap-2 dropdown-item">
+               <i class="ti ti-user fs-6"></i>
+                <p class="mb-0 fs-3">Profil Saya</p>
+              </a>
+              <form action="{{ route('logout') }}" method="POST" style="display:inline;">
+                @csrf
+                <button type="submit" class="btn btn-outline-danger mx-3 mt-3 d-block">Logout</button>
+              </form>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</header>
