5625d530bf2caa7ff331f06c42034cd19063f293
nabil <nabiladitya2203@gmail.com>
2025-06-08 12:22:09 +0700

    Update PendaftaranKegiatan.php

--- app/Models/PendaftaranKegiatan.php
+++ app/Models/PendaftaranKegiatan.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PendaftaranKegiatan extends Model
 {
+    use HasFactory;
+
     protected $table = 'pendaftaran_kegiatan';
     protected $primaryKey = 'id_pendaftaran_kegiatan';
 
@@ -13,4 +16,20 @@ class PendaftaranKegiatan extends Model
         'id_komunitas',
         'id_kegiatan',
     ];
+
+    /**
+     * Relasi ke Komunitas
+     */
+    public function komunitas()
+    {
+        return $this->belongsTo(Komunitas::class, 'id_komunitas', 'id_komunitas');
+    }
+
+    /**
+     * Relasi ke Kegiatan
+     */
+    public function kegiatan()
+    {
+        return $this->belongsTo(Kegiatan::class, 'id_kegiatan', 'id_kegiatan');
+    }
 }
