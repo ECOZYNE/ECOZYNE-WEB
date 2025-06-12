8ce62984f8d93aee9f0115b7d8ab74198031ada8
nabil <nabiladitya2203@gmail.com>
2025-06-12 21:06:31 +0700

    Update Penukaran.php

--- app/Models/Penukaran.php
+++ app/Models/Penukaran.php
@@ -14,4 +14,20 @@ class Penukaran extends Model
         'id_komunita',
         'status_penukaran',
     ];
+
+    public function komunitas()
+{
+    return $this->belongsTo(Komunitas::class, 'id_komunitas');
+}
+
+public function hadiah()
+{
+    return $this->belongsTo(Hadiah::class, 'id_hadiah');
+}
+
+public function transaksi()
+{
+    return $this->hasMany(transaksi_penukaran::class, 'id_penukaran');
+}
+
 }
